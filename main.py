ecf8c06862d0f0300b191ade59b2f8972a7649ac
john <john@jpatmore.plus.com>
2020-12-11 01:20:53 +0000

    Completed

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import pandas
 
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 
+# Create DataFrame from CSV
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(df)
+"""
+   letter      code
+0       A      Alfa
+1       B     Bravo
+2       C   Charlie
+"""
+
+# This doesn't give the format we require:
+# df_dict = df.to_dict()
+# print(df_dict)
+"""{'letter': {0: 'A', 1: 'B', 2: 'C', ... }, 'code': {0: 'Alfa', 1: 'Bravo', 2: 'Charlie', ... }"""
+
+# Create dictionary in the correct format
+nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+# print(nato_dict)
+"""
+{'A': 'Alfa', 'B': 'Bravo', 'C': 'Charlie', ...}
+"""
+
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Type a word : ").upper()
+"""John"""
+code_list = [nato_dict[letter] for letter in word]
+print(code_list)
+"""['Juliet', 'Oscar', 'Hotel', 'November']"""
